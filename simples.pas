c1c7f2274ca4730069d9240976f6dae74a921f5b
Robin Yancey <reyancey@ucdavis.edu>
2021-06-16 17:51:33 -0700

    Update simples.pas

--- simples.pas
+++ simples.pas
@@ -1,8 +1,13 @@
+// SaleItem is holds price values
+// ASale holds item name and attributes
+// SaleItem.sku is an indicator that the configurable comes AFTER (not before)
+
 if SaleItem.product_type='simple' then
   begin
       if (I>0) and (ASale.items[I-1].product_type='configurable') then
       begin
-        SaleItem:=ASale.items[I-1]; //it changes the SaleItem but downwards it consider only the values of it, the name was handled upwards, before the if statement
+        SaleItem:=ASale.items[I-1]; 
+        //it changes the SaleItem but downwards it consider only the values of it, the name was handled upwards, before the if statement
       end else
       if (Length(ASale.items) > I + 1) and (ASale.items[I + 1].product_type = 'configurable') and (ASale.items[I + 1].sku = SaleItem.sku) then
       begin
