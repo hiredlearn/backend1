ea07cfadecc408bd9be9bbb44d4eb93dee79bf4b
Robin Yancey <reyancey@ucdavis.edu>
2021-06-16 17:53:33 -0700

    Update simples.pas

--- simples.pas
+++ simples.pas
@@ -11,7 +11,7 @@ if SaleItem.product_type='simple' then
       end else
       if (Length(ASale.items) > I + 1) and (ASale.items[I + 1].product_type = 'configurable') and (ASale.items[I + 1].sku = SaleItem.sku) then
       begin
-        SaleItem:=ASale.items[I+1]; //it changes the SaleItem but downwards it consider only the values of it, the name was handled upwards, before the if statement
+        SaleItem:=ASale.items[I+1]; 
       end;
     End;
 
